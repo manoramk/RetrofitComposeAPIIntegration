38a7f6e6bf98c453f82023f25bbd0b4ebf4b3da6
kamalamanoramanthena <kamalamanoramanthena@gmail.com>
2023-12-07 20:22:07 +0530

    compose changes

--- app/src/main/java/com/list/retrofitapiintegration/MainActivity.kt
+++ app/src/main/java/com/list/retrofitapiintegration/MainActivity.kt
@@ -2,6 +2,7 @@
 package com.list.retrofitapiintegration
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -19,11 +20,19 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
 import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.retrofit.integration.repository.MainRepository
+import com.retrofit.integration.viewModel.MainViewModel
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -36,12 +45,21 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
 
+    private val factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return MainViewModel(repository = MainRepository()) as T
+        }
+    }
+    private val viewModel: MainViewModel by viewModels{
+        factory
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ApiIntAppTheme()
         }
+
     }
 
     @Composable
@@ -53,17 +71,17 @@ class MainActivity : ComponentActivity() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = apiInt, style = MaterialTheme.typography.headlineMedium)
-            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = apiInt, style = MaterialTheme.typography.bodySmall)
+            Spacer(modifier = Modifier.height(16.dp).padding(10.dp))
             Button(
                 onClick = {
                     apiInt = "Loading..."
-                    CoroutineScope(Dispatchers.IO).launch {
-                        apiInt = fetchWeatherFact()
-                    }
+                    apiInt = fetchWeatherFact()
                 }
             ) {
-                Text("Fetch API ")
+                Text("Fetch GET API ")
+                Spacer(modifier = Modifier.height(16.dp).padding(20.dp)
+                    .verticalScroll(rememberScrollState()))
             }
         }
     }
@@ -78,31 +96,17 @@ class MainActivity : ComponentActivity() {
     }
 
 
-     fun fetchWeatherFact(): String {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.openweathermap.org/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+     private fun fetchWeatherFact(): String {
 
-        val service: WeatherApiService = retrofit.create(WeatherApiService::class.java)
-        var data = "Empty"
-
-        val call: Call<WeatherResponse?>? = service.getWeatherData("CityName", "YOUR_API_KEY")
-        call?.enqueue(object : Callback<WeatherResponse?> {
-            override fun onResponse(call: Call<WeatherResponse?>?, response: Response<WeatherResponse?>) {
-                if (response.isSuccessful()) {
-                    val weatherData: WeatherResponse? = response.body()
-
-                    data = weatherData?.data.toString()
-                } else {
-                    // Handle the error
-                }
-            }
-
-            override fun onFailure(call: Call<WeatherResponse?>?, t: Throwable?) {
-                // Handle network errors
-            }
-        })
+         viewModel.getPost()
+         var data = "No Data"
+        // Observe changes in the LiveData
+         viewModel.post.observe(this, Observer { post ->
+             // Update UI with the post data
+             for (i in 0 .. 1) {
+                 data = post.message?.toString().toString()
+             }
+         })
 
         return data
     }
